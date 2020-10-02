fdf6b7453abe296aed001e154be77482f5bbbe82
Shmuel Kamensky <shmuelkamensky@gmail.com>
2020-10-02 21:19:38 +0300

    using timeit instead of time

--- embedded_python/speed_test.py
+++ embedded_python/speed_test.py
@@ -1,26 +1,29 @@
 import subprocess
 import time
 from subprocess import DEVNULL
+import timeit
 
-s1 = time.time()
-subprocess.run(
-    [
-        "python3.8",
-        "test1.py",
-        "&&",
-        "python3.8",
-        "test2.py",
-        "&&",
-        "python3.8",
-        "test3.py",
-    ],
-    stdout=DEVNULL,
-)
-total1 = time.time() - s1
-s2 = time.time()
-subprocess.run(["./run_py_files", "test1.py", "test2.py", "test3.py"], stdout=DEVNULL)
-total2 = time.time() - s2
+def separate_processes():
+    subprocess.run(
+        [
+            "python3.8",
+            "test1.py",
+            "&&",
+            "python3.8",
+            "test2.py",
+            "&&",
+            "python3.8",
+            "test3.py",
+        ],
+        stdout=DEVNULL,
+    )
+
+def single_embedded_python_process():
+    subprocess.run(["./run_py_files", "test1.py", "test2.py", "test3.py"], stdout=DEVNULL)
+
+multi =  timeit.timeit(separate_processes, number=100)
+single = timeit.timeit(single_embedded_python_process, number=100)
 
 print(
-    f"Running 3 python files in an embedded interpreter is {round(total1/total2,2)}% faster than running the python executable 3 times"
+    f"Running 3 python files in an embedded interpreter is {round(multi/single,2)}% faster than running the python executable 3 times"
 )
